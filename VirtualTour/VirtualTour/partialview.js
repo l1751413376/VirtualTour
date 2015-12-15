039befd30a40414f95596c63b823016f2bf57dfe
ly09614 <ly09614@ly.com>
2015-12-15 15:33:17 +0800

    1

--- VirtualTour/VirtualTour/partialview.js
+++ VirtualTour/VirtualTour/partialview.js
@@ -1,5 +1,6 @@
 ﻿function noready()
 {
+    //检测状态
     if (document.readyState === "complete") {
         setTimeout(ready, 1);
     }
@@ -11,7 +12,6 @@ noready();
 //文档加载完毕
 function ready()
 {
-    //document.getElementById("").insertAdjacentHTML();
     var els = document.getElementsByTagName("script");
     for (var i = 0; i < els.length;i++)
     {
@@ -21,16 +21,19 @@ function ready()
         partialview(item, url);
     }
 }
+//分布视图
 function partialview(element,url)
 {
     var iframe = document.createElement("iframe");
     iframe.setAttribute("src", url);
     iframe.setAttribute("style", "display:none");
+    //设置载入
     iframe.onload = function () {
         element.insertAdjacentHTML("beforeBegin", this.contentDocument.body.innerHTML);
         element.parentNode.removeChild(element);
         document.body.removeChild(iframe);
     }
+    //放到文档中
     document.body.appendChild(iframe);
 }
 
